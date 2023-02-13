d544f724bb2bb1d36a21bae8d31c5b484afff0b2
winstonelim <dnjsdud1106@gmail.com>
2023-02-13 14:42:18 +0900

    SG & Instance

--- tf-ec2-module/module/ec2/sg.tf
+++ tf-ec2-module/module/ec2/sg.tf
@@ -35,10 +35,27 @@ resource "aws_security_group" "ec2_private_sg" {
   description = "ec2-private security group"
   vpc_id      = local.vpc_id
 
+### SSH ###
   ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
+    cidr_blocks      = [local.dmz_subnet[0].cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+### HTTP ###
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "http"
     cidr_blocks      = [local.dmz_subnet[0].cidr_block]
   }
 
