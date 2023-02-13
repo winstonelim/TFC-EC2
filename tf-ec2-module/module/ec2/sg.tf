b779756e05497faf8c75c4a321ebc8aae8c93414
winstonelim <dnjsdud1106@gmail.com>
2023-02-13 20:33:24 +0900

    SG & Instance 2

--- tf-ec2-module/module/ec2/sg.tf
+++ tf-ec2-module/module/ec2/sg.tf
@@ -51,22 +51,6 @@ resource "aws_security_group" "ec2_private_sg" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
-### HTTP ###
-  ingress {
-    from_port        = 80
-    to_port          = 80
-    protocol         = "http"
-    cidr_blocks      = [local.dmz_subnet[0].cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
   tags = {
     Name = "${local.company_name}-${local.server_env}-sg-private"
   }
